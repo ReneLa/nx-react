d077495fe71b253935edd0637ee0e4739a965659
Rene La <renela79@gmail.com>
2022-08-12 00:48:59 +0200

    Game interface added to api repo

--- apps/api/src/app/game.repository.ts
+++ apps/api/src/app/game.repository.ts
@@ -1,9 +1,12 @@
-const games=[
+import { Game } from "@nx-react/api/util-interfaces";
+
+const games:Game[] = [
     {
         id:'billiard',
         name:'Pool Table',
         description:'Pool table with snooker is highly interectual',
         image:'',
+        price:45,
         rating:35.564
     },
     {
@@ -11,6 +14,7 @@ const games=[
         name:'Chess Board',
         description:'It does not only take brains to be a world champion but more than 1000hr of practical work too.',
         image:'',
+        price:10,
         rating:15.765453
     },
     {
@@ -18,6 +22,7 @@ const games=[
         name:'Basketball',
         description:'Huge fan base and so expensive to get facilities',
         image:'',
+        price:89,
         rating:45.42
     },
 ]
